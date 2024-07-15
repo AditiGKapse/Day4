877e6b4218ed911283a41e0616c09f4a23768fbb
AditiGKapse <aditikapse92@gmail.com>
2024-07-15 20:49:57 +0530

    Add files via upload

---
+++ src/constructor/MyClass.java
@@ -0,0 +1,31 @@
+package constructor;
+
+public class MyClass {
+	private static MyClass obj=new MyClass();
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	private MyClass()
+	{
+		System.out.println("MyClass obhject created ");
+	}
+
+	public static MyClass getObject()
+	{
+		return obj;
+	}
+
+	@Override
+	public String toString() {
+		return "MyClass [id=" + id + "]";
+	}
+
+}
